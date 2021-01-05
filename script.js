61cf4dc58d7f7509348c41a9a44f755c894f9d45
adam1987j <76119318+adam1987j@users.noreply.github.com>
2021-01-05 10:42:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 var interval;
 function counting() {
-		var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	      document.getElementById("chat").innerHTML = this.responseText;
-	    }
-	  };
-	  xhttp.open("POST", "load_mes.php", true);
-	  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	  var me1 = document.getElementById("selectUser").options[document.getElementById("selectUser").selectedIndex].value;
-		xhttp.send("me=" + me1 + "&who=" + document.getElementById("chat").value);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("chat").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "load_mes.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  var me1 = document.getElementById("selectUser").options[document.getElementById("selectUser").selectedIndex].value;
+  xhttp.send("me=" + me1 + "&who=" + document.getElementById("chat").value);
 }
 function loadMsg(me, who) {
-	clearInterval(interval);
-	$chat = document.getElementById('chat');
-	document.getElementById("message2").style.display = "block";
-	$chat.value = who;
-	document.getElementById("h1chat").innerHTML = "chat with <span id=chattitle>"+who+"</span>";
-	$chat.innerHTML = "";
-	var xhttp = new XMLHttpRequest();
+  clearInterval(interval);
+  $chat = document.getElementById('chat');
+  document.getElementById("message2").style.display = "block";
+  $chat.value = who;
+  document.getElementById("h1chat").innerHTML = "chat with <span id=chattitle>"+who+"</span>";
+  $chat.innerHTML = "";
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("chat").innerHTML += this.responseText;
@@ -30,7 +30,7 @@ function loadMsg(me, who) {
   interval = setInterval(counting, 1000);
 };
 function sendMsg(me,who,text) {
-	var xhttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("chat").innerHTML += this.responseText;
@@ -41,18 +41,18 @@ function sendMsg(me,who,text) {
   xhttp.send("me="+me+"&who="+who+"&text="+text);
 };
 function reloadUser() {
-	clearInterval(interval);
-	document.getElementById("message2").style.display = "none";
-	document.getElementById("users2").innerHTML = "";
-	var x = document.getElementById("selectUser");
-	Array.from(document.querySelector("#selectUser").options).forEach(function(option_element) {
-		if (!option_element.selected)
-		{
-			var para = document.createElement("P");
-			para.value = option_element.value;
-			para.onclick = function() {loadMsg(x.options[x.selectedIndex].value, option_element.value)};
-			para.appendChild(document.createTextNode(option_element.text));
-		  document.getElementById("users2").appendChild(para);
-	  }
-	});
+  clearInterval(interval);
+  document.getElementById("message2").style.display = "none";
+  document.getElementById("users2").innerHTML = "";
+  var x = document.getElementById("selectUser");
+  Array.from(document.querySelector("#selectUser").options).forEach(function(option_element) {
+    if (!option_element.selected)
+    {
+      var para = document.createElement("P");
+      para.value = option_element.value;
+      para.onclick = function() {loadMsg(x.options[x.selectedIndex].value, option_element.value)};
+      para.appendChild(document.createTextNode(option_element.text));
+      document.getElementById("users2").appendChild(para);
+    }
+  });
 }
