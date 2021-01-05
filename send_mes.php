48eddb13c95c5740bc3daf72fcb6efff21c7ba7b
adam1987j <76119318+adam1987j@users.noreply.github.com>
2021-01-05 10:34:01 +0100

    Update send_mes.php

--- send_mes.php
+++ send_mes.php
@@ -2,16 +2,7 @@
 	$me = $_POST['me'];
 	$who = $_POST['who'];
 	$text = $_POST['text'];
-	$xml = new DOMDocument();
-  $xml_message = $xml->createElement("Message");
-  $xml_me = $xml->createElement("me", $me);
-  $xml_who = $xml->createElement("who", $who);
-  $xml_text = $xml->createElement("text", $text);
-  $xml_message->appendChild( $xml_me );
-  $xml_message->appendChild( $xml_who );
-  $xml_message->appendChild( $xml_text );
-  $xml->appendChild( $xml_message );
-  $fileName = "message.xml";
+  	$fileName = "message.xml";
 	if (!file_exists($fileName)) {
 	    echo "Cannot find file.";
 	} else {
@@ -19,5 +10,5 @@
 	}
 	$str = "$me|||$who|||$text".PHP_EOL;
 	fwrite($file, $str);
-  fclose($file);
+  	fclose($file);
 ?>
